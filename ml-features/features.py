bb5f18aec8a081046afeedce87c896ccd9f08232
SamHaeck <samueldhaeck@gmail.com>
2024-03-13 19:07:59 +0100

    feature functions

--- ml-features/features.py
+++ ml-features/features.py
@@ -1,44 +1,68 @@
+
 import re
+import numpy as np
 
-class Features:
-    def __init__(self, input):
-        self.features = []
-        self.nr_chars = len(input)
-        self.features.append({"nr_of_words", self.nr_of_words(input)})
-        self.features.append({"nr_of_lines", self.nr_of_lines(input)})
-        self.features.append({"nr_of_urls", self.nr_of_urls(input)})
-        self.features.append({"nr_of_ip_address", self.nr_of_ip_address(input)})
-        
-        self.features.append({"ratio_square_bracket_sign", self.ratio_char(self.nr_of_square_bracket_signs(input))})
-        self.features.append({"ratio_equals_sign", self.ratio_char(self.nr_of_plus_signs(input))})
-        self.features.append({"ratio_plus_sign", self.ratio_char(self.nr_of_equal_signs(input))})
+# 1. Number of words in file
+def count_words(file_content):
+    words = file_content.split()
+    return ("number_of_words", len(words))
 
-    def nr_of_words(self, input):
-        return len(input.strip().split())
-    
-    def nr_of_lines(self, input):
-        return len(input.splitlines())
-    
-    def nr_of_urls(self, input):
-        url_pattern = re.compile(r'\b((?:https?|ftp|file):\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|])')
-        return len(url_pattern.findall(input))
-    
-    def nr_of_ip_address(self, input):
-        ip_pattern = re.compile(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b')
-        return len(ip_pattern.findall(input))
+# 2. Number of lines in file
+def count_lines(file_content):
+    lines = file_content.split("\n")
+    return ("number_of_lines", len(lines))
 
-    def nr_all_chars(self, input):
-        return input.count()
+# 3. Number of URLs in file
+def count_urls(file_content):
+    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', file_content)
+    return ("number_of_urls", len(urls))
 
-    def nr_of_square_bracket_signs(self, input):
-        return input.count("[") + input.count("]")
+# 4. Number of IP addresses in file
+def count_ips(file_content):
+    ips = re.findall(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', file_content)
+    return ("number_of_ip_addresses", len(ips))
 
-    def nr_of_plus_signs(self, input):
-        return input.count("+")
+# 5. Revised Statistics on ratio of square brackets per line
+def square_brackets_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("[") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "square_brackets_mean": mean,
+        "square_brackets_std_dev": std_dev,
+        "square_brackets_third_quartile": third_quartile,
+        "square_brackets_max_value": max_value
+    }
 
-    def nr_of_equal_signs(self, input):
-        return input.count("=")
-    
-    def ratio_char(self, nr):
-        return nr / self.nr_chars
+# 6. Revised Statistics on ratio of equal signs per line
+def equal_signs_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("=") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "equal_signs_mean": mean,
+        "equal_signs_std_dev": std_dev,
+        "equal_signs_third_quartile": third_quartile,
+        "equal_signs_max_value": max_value
+    }
 
+# 7. Revised Statistics on ratio of plus signs per line
+def plus_signs_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("+") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "plus_signs_mean": mean,
+        "plus_signs_std_dev": std_dev,
+        "plus_signs_third_quartile": third_quartile,
+        "plus_signs_max_value": max_value
+    }
