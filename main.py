48da66b7a01df1a1b540a70c14620cd6e325303f
Robert Thomas <thomcrnl@gmail.com>
2024-03-13 15:39:52 +0000

    Add structure

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+def run_yar_files(folder_path):
+    for root, dirs, files in os.walk(folder_path):
+        for file in files:
+            if file.endswith(".yar"):
+                yar_file_path = os.path.join(root, file)
+                #subprocess.call(["your_command_here", yar_file_path])
+
+def feature_placeholder_1(content):
+    return ()
+
+def feature_placeholder_2(content):
+    return ()
+
+def feature_placeholder_3(content):
+    return ()
+
+
+def main():
+    print("Start of Program")
+    folder_path = "/path/to/your/folder"
+
+
+if __name__ == "__main__":
+    main()
