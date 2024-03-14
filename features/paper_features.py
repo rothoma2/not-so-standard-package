2b1fbc7d64c69b61460166cf73e3c2c34be5473f
SamHaeck <samueldhaeck@gmail.com>
2024-03-14 02:15:04 +0100

    refactored main

---
+++ features/paper_features.py
@@ -0,0 +1,86 @@
+def feature_count_words(file_content):
+    words = file_content.split()
+    return {"count_words" : len(words) }
+
+def feature_count_lines(file_content):
+    lines = file_content.split("\n")
+    return {"number_of_lines": len(lines)}
+
+def feature_count_urls(file_content):
+    urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', file_content)
+    return {"number_of_urls": len(urls)}
+
+def feature_count_ips(file_content):
+    ips = re.findall(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', file_content)
+    return {"number_of_ip_addresses": len(ips)}
+
+
+def feature_square_brackets_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("[") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "square_brackets_mean": mean,
+        "square_brackets_std_dev": std_dev,
+        "square_brackets_third_quartile": third_quartile,
+        "square_brackets_max_value": max_value
+    }
+
+
+def feature_equal_signs_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("=") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "equal_signs_mean": mean,
+        "equal_signs_std_dev": std_dev,
+        "equal_signs_third_quartile": third_quartile,
+        "equal_signs_max_value": max_value
+    }
+
+def feature_plus_signs_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("+") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "plus_signs_mean": mean,
+        "plus_signs_std_dev": std_dev,
+        "plus_signs_third_quartile": third_quartile,
+        "plus_signs_max_value": max_value
+    }
+
+def feature_underscore_signs_stats(file_content):
+    lines = file_content.split("\n")
+    ratios = [line.count("_") / len(line) for line in lines if len(line) > 0]
+    mean = np.mean(ratios)
+    std_dev = np.std(ratios)
+    third_quartile = np.quantile(ratios, 0.75)
+    max_value = max(ratios)
+    return {
+        "underscore_signs_mean": mean,
+        "underscore_signs_std_dev": std_dev,
+        "underscore_signs_third_quartile": third_quartile,
+        "underscore_signs_max_value": max_value
+    }
+
+def obfuscated_code_python(file_content):
+    matches = re.findall(r'(?s)\"\"*[^\']*\"\"*|\'\'*[^\']*\'\'*', file_content)
+    counter = 0
+    stripped_matches = [match.strip('\"\'') for match in matches]
+    for match in stripped_matches:
+        try:
+            decoded_data = base64.b64decode(match.encode())
+            module = ast.parse(io.BytesIO(decoded_data).read().decode())
+            counter += 1
+        except(SyntaxError, UnicodeDecodeError, binascii.Error):
+            pass
+    return {"obfuscated_code_python": counter}
---
+++ util.py
@@ -0,0 +1,109 @@
+import argparse
+import os
+import subprocess
+from pprint import pprint
+import re
+import numpy as np
+import base64
+import ast
+import io
+import binascii
+import traceback
+import sys
+
+from features.features import SnippetStats
+from features.features_model import Features
+from features import yara_features
+from features.paper_features import *
+
+class Util:
+    def __init__(self):
+        parser = argparse.ArgumentParser(description="Scan a folder using YARA")
+        parser.add_argument("folder", help="Folder path to scan")
+        self.args = parser.parse_args()
+
+        yara_path = "./yara"
+        self.yara_rules = yara_features.load_yara_rules(yara_path)
+    
+    def get_args(self):
+        return self.args
+    
+    def folder_exists(self):
+        if not os.path.isdir(self.args.folder):
+            raise Exception("Error: Folder does not exist.")
+    
+    def list_python_files(self):
+        python_files = []
+
+        for root, dirs, files in os.walk(self.args.folder):
+            for file in files:
+                if file.endswith('.py'):
+                    python_files.append(os.path.join(root, file))
+
+        return python_files
+    
+    def generate_features(self, python_files):
+        results = []
+        for file in top_python_files:
+
+            with open(file, 'r') as file_to_be_read:
+                try:
+                    file_content = file_to_be_read.read()
+                    result = dict()
+                    parts = file.split('/')
+                    last_part = parts[-1]
+                    result["file_name"] = last_part
+
+                    if file_content:
+                        shanon_results = self.generate_statistic_features(file_content)
+                        result.update(shanon_results)
+
+                    stripped_file = file_content.replace(" ", "")
+                    stripped_file = stripped_file.replace("\n", "")
+
+                    if stripped_file:
+                        result.update(self.generate_paper_features(file_content))
+                        result.update(self.generate_features(self.yara_rules, file))
+                        results.append(result)
+                except:
+                    print("An exception occurred:", sys.exc_info()[0])  # Print exception info
+                    traceback.print_exc()  # This will print the stack trace to stderr
+        return results
+                
+    def generate_statistic_features(self, file_content):
+        snippet_stats = SnippetStats()
+        snippet_stats.set_snippet(file_content)
+        snippet_stats.line_entropy()
+        
+        shanon_results = {
+        "shanon_entropy__mean":  snippet_stats.shannon_entropy__mean(),
+        "shanon_entropy__median":  snippet_stats.shannon_entropy__median(),
+        "shanon_entropy__variance":  snippet_stats.shannon_entropy__variance(),
+        "shanon_entropy__max":  snippet_stats.shannon_entropy__max(),
+        "shanon_entropy__1Q":  snippet_stats.shannon_entropy__1Q(),
+        "shanon_entropy__3Q":  snippet_stats.shannon_entropy__3Q(),
+        "shanon_entropy__outliers":  snippet_stats.shannon_entropy__outliers(),
+        }
+    
+    def generate_yara_features(self, yara_rules, file):
+        result = dict()
+        for rule in yara_rules:
+            yara_result = yara_features.run_yara_rule(rule, file)
+            result.update(yara_result)
+        return result
+    
+    def generate_paper_features(self, file_content):
+        result = dict()
+        file_content = str(file_content)
+        result.update(feature_count_words(file_content))
+        result.update(feature_count_lines(file_content))
+        result.update(feature_count_urls(file_content))
+        result.update(feature_count_ips(file_content))
+        result.update(feature_square_brackets_stats(file_content))
+        result.update(feature_equal_signs_stats(file_content))
+        result.update(feature_plus_signs_stats(file_content))
+        result.update(obfuscated_code_python(file_content))
+        return result
+
+
+            
